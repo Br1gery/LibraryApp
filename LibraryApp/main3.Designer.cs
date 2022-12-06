374b9b4cfd247d1c3dcb99b5f433d05a70da78c5
Bogdanov-AS <Bogdanov-AS@SRV-002-254>
2022-12-06 10:21:02 +0500

    main3 changed

--- LibraryApp/main3.Designer.cs
+++ LibraryApp/main3.Designer.cs
@@ -49,11 +49,12 @@ namespace LibraryApp
             this.id_text3 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.id_text2 = new System.Windows.Forms.TextBox();
-            this.usersDataBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.loginDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.passwordDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.roleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.usersDataBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.usersDataBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -217,7 +218,7 @@ namespace LibraryApp
             this.delete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.delete.ForeColor = System.Drawing.Color.Black;
-            this.delete.Location = new System.Drawing.Point(547, 385);
+            this.delete.Location = new System.Drawing.Point(547, 365);
             this.delete.Name = "delete";
             this.delete.Size = new System.Drawing.Size(232, 38);
             this.delete.TabIndex = 168;
@@ -228,7 +229,7 @@ namespace LibraryApp
             // id_text3
             // 
             this.id_text3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.id_text3.Location = new System.Drawing.Point(615, 341);
+            this.id_text3.Location = new System.Drawing.Point(615, 321);
             this.id_text3.Name = "id_text3";
             this.id_text3.ReadOnly = true;
             this.id_text3.Size = new System.Drawing.Size(88, 26);
@@ -238,7 +239,7 @@ namespace LibraryApp
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(648, 314);
+            this.label7.Location = new System.Drawing.Point(648, 294);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(25, 24);
             this.label7.TabIndex = 166;
@@ -253,10 +254,6 @@ namespace LibraryApp
             this.id_text2.Size = new System.Drawing.Size(88, 26);
             this.id_text2.TabIndex = 169;
             this.id_text2.Visible = false;
-            // 
-            // usersDataBindingSource
-            // 
-            this.usersDataBindingSource.DataSource = typeof(LibraryApp.UsersData);
             // 
             // idDataGridViewTextBoxColumn
             // 
@@ -282,12 +279,27 @@ namespace LibraryApp
             this.roleDataGridViewTextBoxColumn.HeaderText = "Роль";
             this.roleDataGridViewTextBoxColumn.Name = "roleDataGridViewTextBoxColumn";
             // 
+            // usersDataBindingSource
+            // 
+            this.usersDataBindingSource.DataSource = typeof(LibraryApp.UsersData);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(527, 423);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(264, 15);
+            this.label8.TabIndex = 170;
+            this.label8.Text = "Роли (1 - библиотекарь 2 - студент 3-админ)";
+            // 
             // main3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.id_text2);
             this.Controls.Add(this.delete);
             this.Controls.Add(this.id_text3);
@@ -343,5 +355,6 @@ namespace LibraryApp
         private System.Windows.Forms.DataGridViewTextBoxColumn loginDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn passwordDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn roleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label8;
     }
 }
